bfaf3c44c3b00845e4d8a9b743c881b0b02e27be
Allan <allan@datadecision.ai>
2024-10-07 06:04:42 +0800

    處理解析動態docstring以及同時考慮docstring以及inspect

--- paradoxism/base/agent.py
+++ paradoxism/base/agent.py
@@ -2,14 +2,17 @@ import hashlib
 import threading
 import time
 import uuid
+import re
 from functools import wraps
-from typing import Callable
-
+from typing import Callable,Iterable,Dict,Any
+import inspect
 from openai import OpenAI
-
+from collections import OrderedDict
 from paradoxism.base.perfm import PerformanceCollector
 from paradoxism.utils.utils import *
 from paradoxism.utils.docstring_utils import *
+from paradoxism.ops.convert import *
+from paradoxism.llm import *
 
 # 建立全域的 PerformanceCollector 實例，保證所有地方都能使用這個實例
 collector = PerformanceCollector()
@@ -23,45 +26,20 @@ def get_current_executor():
     return getattr(_thread_local, 'executor', None)
 
 
-class LLMClient:
-    """封裝 OpenAI LLM 客戶端的類別。"""
-
-    def __init__(self, model: str, system_prompt: str, temperature: float = 0.2, **kwargs):
-
-        self.client=OpenAI()
-        self.model = model
-        self.system_prompt = system_prompt
-        self.temperature = temperature
-        self.extra_params = kwargs
-
-    def generate(self, prompt: str) -> str:
-        """生成 LLM 的回應。"""
-
-        response = self.client.chat.completions.create(
-            model=self.model,
-            messages=[
-                {"role": "system", "content": self.system_prompt},
-                {"role": "user", "content": prompt}
-            ],
-            temperature=self.temperature
-
-        )
-        return response.choices[0].message.content.strip()
-
-
 def generate_agent_key(system_prompt: str, static_instruction: str, func_code: str):
     """基於 system_prompt, static_instruction 及函數邏輯生成唯一的哈希 key"""
     hash_input = system_prompt + static_instruction + func_code
     return hashlib.sha256(hash_input.encode()).hexdigest()
 
 
-def agent(model: str, system_prompt: str, temperature: float = 0.7, **kwargs):
+def agent(model: str, system_prompt: str, temperature: float = 0.7,stream=False, **kwargs):
     """
     @agent 裝飾器，用於標記任務的最小單位。
     Args:
-        model: 使用的模型名稱，例如 'gpt-4'
+        provider_or_model_name: 使用的llm提供者或是模型名稱，例如 'openai','gpt-4'
         system_prompt: 系統提示語
         temperature: 溫度參數，控制回應的隨機性
+        stream: 是否為stream輸出
         **kwargs: 其他額外參數
 
     Returns:
@@ -70,25 +48,57 @@ def agent(model: str, system_prompt: str, temperature: float = 0.7, **kwargs):
 
     def decorator(func: Callable):
 
-        func.llm_client = LLMClient(model, system_prompt, temperature, **kwargs)
+        func.llm_client =get_llm(model, system_prompt, temperature, **kwargs)
+        if func.__doc__ is None:
+            # 使用 re.search 來提取第一個被 """ 夾住的區塊
+            match = re.search( r'def\s+\w+\s*\(.*?\):\s*f?"""\s*([\s\S]*?)\s*"""', inspect.getsource(func))
+            if match:
+                func.__doc__ = match.group(1)
+            else:
+                func.__doc__=''
 
-        func.static_instruction =parse_docstring(func.__doc__)['static_instruction'] or ""
         _thread_local.llm_client =func.llm_client
-        _thread_local.static_instruction = parse_docstring(func.__doc__)['static_instruction'] or ""
 
         @wraps(func)
         def wrapper(*args, **kwargs_inner):
             instance_id = str(uuid.uuid4())
 
-            # 基於 system_prompt, static_instruction 和代碼邏輯生成唯一的 agent key
-            func_code = func.__code__.co_code.decode('latin1')  # 使用函數的原始字節碼生成唯一 key
-            agent_key = generate_agent_key(system_prompt, func.static_instruction, func_code)
+            # 產生inputs_dict
+            inputs_dict = OrderedDict()
+            signature = inspect.signature(func)
+            for i,(param_name, param) in enumerate(signature.parameters.items()):
+                if len(args) > i:
+                    inputs_dict[param_name] = {'arg_name':param_name,'arg_value': args[i], 'arg_type': param.annotation.__name__ if param.annotation else None}
+                elif param_name in kwargs_inner:
+                    inputs_dict[param_name] = {'arg_name': param_name, 'arg_value': kwargs_inner['param_name'], 'arg_type': param.annotation.__name__ if param.annotation else None}
+                elif param.default is not inspect.Parameter.empty:
+                    inputs_dict[param_name] = {'arg_name':param_name,'arg_value':  str(param.default), 'arg_type': param.annotation.__name__ if param.annotation else None}
+                else:
+                    inputs_dict[param_name]={'arg_name':param_name,'arg_value':'none',  'arg_type': param.annotation.__name__ if param.annotation else None}
+            _thread_local.input_args = inputs_dict
 
+            variables_need_to_replace =list(set(re.findall(r'{(.*?)}', func.__doc__)))
+            if len(variables_need_to_replace) and len([item for item in variables_need_to_replace if item in inputs_dict])== len(variables_need_to_replace):
+                docstring= func.__doc__.format(**{k:inputs_dict[k]['arg_value'] for k in variables_need_to_replace})
+            else:
+                docstring = func.__doc__
+            parsed_results =parse_docstring(docstring)
+
+            # 基於 system_prompt, static_instruction 和代碼邏輯生成唯一的 agent key
+
+            signature = inspect.signature(func)
+            return_annotation = get_type_hints(func).get('return')
             start_time = time.time()  # 記錄開始時間
             executor = get_current_executor()
             _thread_local.llm_client = func.llm_client
-            _thread_local.static_instruction = func.static_instruction
-            result=None
+            _thread_local.static_instruction = parsed_results['static_instruction']
+            _thread_local.returns =parsed_results[ 'return']
+
+            func_code = func.__code__.co_code.decode('latin1')  # 使用函數的原始字節碼生成唯一 key
+            agent_key = generate_agent_key(system_prompt, _thread_local.static_instruction, func_code)
+
+
+            result = None
             if executor:
                 # 委託給 FlowExecutor 執行
 
@@ -96,6 +106,12 @@ def agent(model: str, system_prompt: str, temperature: float = 0.7, **kwargs):
             else:
                 # 正常執行
                 result= func(*args, **kwargs_inner)
+
+            if  return_annotation=='str' or (not isinstance(result, Iterable) and len( _thread_local.returns)==1 and return_annotation  in target_types):
+                result = force_cast(result, return_annotation)
+            elif not isinstance(result, Iterable) and len( _thread_local.returns)==1 and  _thread_local.returns['return_type'] in target_types:
+                result =force_cast(result,_thread_local.returns['return_type'])
+
 
             end_time = time.time()  # 記錄結束時間
             execution_time = end_time - start_time  # 計算執行時間
