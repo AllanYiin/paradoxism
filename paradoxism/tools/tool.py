f24d56c8742e051fdecf8f74508be708f6120f42
Allan <allan@datadecision.ai>
2024-12-19 11:41:46 +0800

    修正不正確的indent

--- paradoxism/tools/tool.py
+++ paradoxism/tools/tool.py
@@ -1,19 +1,20 @@
 import openai
 from functools import wraps
-from utils.regex_utils  import extract_json
+from utils.regex_utils import extract_json
 import threading
 from paradoxism.base.agent import _thread_local
 from paradoxism.llm import *
 from paradoxism.utils.docstring_utils import *
 from paradoxism.utils.input_dict_utils import *
 from paradoxism.ops.base import reference
+
+
 class BaseTool:
-    def __init__(self, model="gpt-4o", system_prompt="你是一個擅長工具調用的超級幫手",temperature=0.1, **kwargs):
+    def __init__(self, model="gpt-4o", system_prompt="你是一個擅長工具調用的超級幫手", temperature=0.1, **kwargs):
         self.model = model
         self.system_prompt = system_prompt
-        self.llm_client =get_llm(model, system_prompt, temperature, **kwargs)
-        self.base_func=None
-
+        self.llm_client = get_llm(model, system_prompt, temperature, **kwargs)
+        self.base_func = None
 
     def generate_tool_args(self, tool_name, docstring, input_kwargs):
         """
@@ -25,7 +26,7 @@ class BaseTool:
         Returns:
             生成的工具引數 dict
         """
-        input_args=_thread_local.input_args
+        input_args = _thread_local.input_args
         prompt = f"""
         你將調用一個工具，工具名稱是 "{tool_name}"，這個工具的功能以及所需引數如下：
         # 工具說明
@@ -54,13 +55,13 @@ class BaseTool:
             temperature=0.0
 
         )
-        tool_args =response.choices[0].message.content
+        tool_args = response.choices[0].message.content
 
         print('tool_args生成完畢!\n', tool_args, flush=True)
 
         return tool_args
 
-    def __call__(self,input_kwargs,**kwargs):
+    def __call__(self, input_kwargs, **kwargs):
         """
         收集上下文並生成最終的工具引數，然後調用原始函數。
         Args:
@@ -70,7 +71,7 @@ class BaseTool:
         Returns:
             工具函數的執行結果
         """
-        tool_name =self.base_func.__name__
+        tool_name = self.base_func.__name__
         docstring = self.base_func.__doc__ or ""
         # 生成引數
         tool_args = self.generate_tool_args(tool_name, docstring, input_kwargs)
@@ -81,7 +82,8 @@ class BaseTool:
         return result
 
 
-def tool(model: str, system_prompt: str="你是一個擅長工具調用的超級幫手", temperature: float = 0.7,stream=False, **kwargs):
+def tool(model: str, system_prompt: str = "你是一個擅長工具調用的超級幫手", temperature: float = 0.7, stream=False,
+         **kwargs):
     """
     @tool 裝飾器，用於將函數封裝為工具，並使用 LLM 來生成輸入引數。他與Agent的差別在於，Agent一定是有包含語言模型的操作，且有固定形式的輸入。
     而tool則是類似tools calls的概念，但是他是必定執行，它主要執行本體未必需要語言模型，只不過它透過input_kwargs，也就是將一個dict盛裝所有可能有輸入值訊息的上文傳入，語言模型會自動整理輸入值
@@ -91,17 +93,13 @@ def tool(model: str, system_prompt: str="你是一個擅長工具調用的超級
     def decorator(func):
         # 創建 BaseTool 實例
         lock = threading.Lock()
-        base_tool = BaseTool(model=model, system_prompt=system_prompt,temperature=temperature, **kwargs)
+        base_tool = BaseTool(model=model, system_prompt=system_prompt, temperature=temperature, **kwargs)
         base_tool.base_func = func
         func.llm_client = base_tool.llm_client
         with lock:
             _thread_local.llm_client = base_tool.llm_client
         if func.__doc__ is None:
             func.__doc__ = extract_docstring(func)
-
-
-
-
 
         @wraps(func)
         def wrapper(input_kwargs):
@@ -110,7 +108,7 @@ def tool(model: str, system_prompt: str="你是一個擅長工具調用的超級
             """
             # 通過 BaseTool 來生成最終的引數並調用工具函數
 
-            parsed_results =get_input_dict(func)
+            parsed_results = get_input_dict(func)
 
             with lock:
                 _thread_local.llm_client = base_tool.llm_client
